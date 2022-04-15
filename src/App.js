ece564fdaaa9e457b73a9d231c90f35439f73362
elis-11 <eliza.arzanukaeva@gmail.com>
2022-04-15 18:04:12 +0200

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className="App">Trlala</div>;
+  return (
+    <div className="App">
+      <h1>Tralala</h1>
+    </div>
+  );
 }
 
 export default App;
