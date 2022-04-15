654737d4a405b0fdb06c0ab0eb6a15a84fd8b6c2
elis-11 <eliza.arzanukaeva@gmail.com>
2022-04-15 19:46:58 +0200

    Add & Delete functions

--- src/App.js
+++ src/App.js
@@ -29,12 +29,87 @@ function App() {
       city: "Hamburg",
     },
   ]);
+  const [newUser, setNewUser] = useState({
+    name: "",
+    profession: "",
+    hobby: "",
+    email: "",
+    city: "",
+  });
+
+  const addUser = () => {
+    const userNewState = {
+      id: Date.now().toString(),
+      name: newUser.name,
+      profession: newUser.profession,
+      hobby: newUser.hobby,
+      email: newUser.email,
+      city: newUser.city,
+    };
+    setUsers([...users, userNewState]);
+    setNewUser({
+      ...newUser,
+      name: "",
+      profession: "",
+      hobby: "",
+      email: "",
+      city: "",
+    });
+  };
+  const handleAddUser = (e) => {
+    setNewUser({ ...newUser, [e.target.name]: e.target.value });
+  };
+
+  const handleDelete = (id)=>{
+    const deleteUser= users.filter(user=>user.id !== id)
+    setUsers(deleteUser);
+  }
 
   return (
     <div className="App">
-      <h1>Tralala</h1>
       <div className="container">
-        <UsersList users={users} />
+        <h1>Tralala</h1>
+
+        <div className="add">
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={newUser.name}
+            onChange={handleAddUser}
+            autoFocus
+          />
+          <input
+            type="text"
+            name="profession"
+            placeholder="Profession"
+            value={newUser.profession}
+            onChange={handleAddUser}
+          />
+          <input
+            type="text"
+            name="hobby"
+            placeholder="Hobby"
+            value={newUser.hobby}
+            onChange={handleAddUser}
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={newUser.email}
+            onChange={handleAddUser}
+          />
+          <input
+            type="text"
+            name="city"
+            placeholder="City"
+            value={newUser.city}
+            onChange={handleAddUser}
+          />
+          <button onClick={addUser}>Add</button>
+        </div>
+        <UsersList users={users} handleDelete={handleDelete} />
       </div>
     </div>
   );
--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
 
-const UsersList = ({users}) => {
+const UsersList = ({users, handleDelete}) => {
   return (
     <div className="users">
     {users.map((user) => (
@@ -10,6 +10,10 @@ const UsersList = ({users}) => {
         <div className='item'>{user.hobby}</div>
         <div className='item'>{user.email}</div>
         <div className='item'>{user.city}</div>
+        <div className="icons">
+        <FaEdit className="icon" role="button" tabIndex="0" />
+        <FaTrashAlt className="icon" role="button" tabIndex="0" onClick={() => handleDelete(user.id)} />
+        </div>
       </div>
     ))}
   </div>
