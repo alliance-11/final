09dcf6b6c3c115161eea404afdf0456c803469fe
elis-11 <eliza.arzanukaeva@gmail.com>
2022-04-18 09:37:11 +0200

    AddForm.jsx- 'Components'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.scss";
+import AddForm from "./components/AddForm";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import UsersList from "./components/UsersList";
@@ -71,45 +72,11 @@ function App() {
     <div className="App">
       <Header title="Final Project" />
       <div className="container">
-        <div className="add">
-          <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            value={newUser.name}
-            onChange={handleAddUser}
-            autoFocus
-          />
-          <input
-            type="text"
-            name="profession"
-            placeholder="Profession"
-            value={newUser.profession}
-            onChange={handleAddUser}
-          />
-          <input
-            type="text"
-            name="hobby"
-            placeholder="Hobby"
-            value={newUser.hobby}
-            onChange={handleAddUser}
-          />
-          <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            value={newUser.email}
-            onChange={handleAddUser}
-          />
-          <input
-            type="text"
-            name="city"
-            placeholder="City"
-            value={newUser.city}
-            onChange={handleAddUser}
-          />
-          <button onClick={addUser}>Add</button>
-        </div>
+        <AddForm
+          newUser={newUser}
+          handleAddUser={handleAddUser}
+          addUser={addUser}
+        />
         {users.length ? (
           <UsersList users={users} handleDelete={handleDelete} />
         ) : (
@@ -119,7 +86,7 @@ function App() {
         )}
       </div>
       <Footer length={users.length} />
- {/* {users.length} List {users.length === 1 ? "User" : "Users"} */}
+      {/* {users.length} List {users.length === 1 ? "User" : "Users"} */}
     </div>
   );
 }
