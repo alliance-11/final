bca7b13611cb4ad15f6fb47118d4a2e3910b3738
elis-11 <eliza.arzanukaeva@gmail.com>
2022-04-26 13:55:44 +0200

    Added new functionality.

--- src/components/users/UsersList.jsx
+++ src/components/users/UsersList.jsx
@@ -1,10 +1,15 @@
 import { UserItem } from "./UserItem";
 
-export const UsersList = ({ users, handleDelete,editUser }) => {
+export const UsersList = ({ users, handleDelete, editUser }) => {
   return (
     <div className="users">
       {users.map((user) => (
-        <UserItem key={user.id} user={user} handleDelete={handleDelete} editUser={editUser} />
+        <UserItem
+          key={user.id}
+          user={user}
+          handleDelete={handleDelete}
+          editUser={editUser}
+        />
       ))}
     </div>
   );
