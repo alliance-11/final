f87708e697919dab9ecf342388b409407879bc26
“elis-11” <eliza.arzanukaeva@gmail.com>
2022-05-03 16:53:56 +0200

    Modified document.

---
+++ src/components/teachers/Filter.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Filter = () => {
+  return (
+    <div>Filter</div>
+  )
+}
--- src/components/teachers/TeachersList.jsx
+++ src/components/teachers/TeachersList.jsx
@@ -1,15 +1,10 @@
 import { TeacherItem } from "./TeacherItem";
 
-export const TeachersList = ({
-  filteredTeachers,
-}) => {
+export const TeachersList = ({ filteredTeachers }) => {
   return (
     <div className="teachers">
       {filteredTeachers.map((teacher) => (
-        <TeacherItem
-          key={teacher.id}
-          teacher={teacher}
-        />
+        <TeacherItem key={teacher.id} teacher={teacher} />
       ))}
     </div>
   );
